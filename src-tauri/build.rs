e191ea765f7863b3188d30ba50ba9fe3ff9089b6
tomjiu <1937001877@qq.com>
2026-06-08 22:47:41 +0800

    ci: fix rust test harness on windows

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,10 @@
 fn main() {
     tauri_build::build();
+
+    #[cfg(target_os = "windows")]
+    {
+        println!(
+            "cargo:rustc-link-arg-tests=/MANIFESTDEPENDENCY:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'"
+        );
+    }
 }
